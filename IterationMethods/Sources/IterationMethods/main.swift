ede479677a4750abf331c21aec39a62e766ff326
shgew <thegamedar@gmail.com>
2020-05-31 18:47:55 +0300

    Init main.swift

--- IterationMethods/Sources/IterationMethods/main.swift
+++ IterationMethods/Sources/IterationMethods/main.swift
@@ -5,4 +5,16 @@
 //  Created by Gleb Shevchenko on 5/31/20.
 //
 
-import Foundation
+import IterationMethodsCore
+
+let separator = "================================"
+
+let x = 0.96
+let y = 0.27
+let precision = 10e-5
+
+fixedPointIteration(x: x, y: y, precision: precision)
+print(separator)
+gaussSeidel(x: x, y: y, precision: precision)
+print(separator)
+newton(x: x, y: y, precision: precision)
