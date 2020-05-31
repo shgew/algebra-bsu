81b3860c9661678ff97854aa6e99df6f0251c713
shgew <thegamedar@gmail.com>
2020-05-31 18:10:42 +0300

    Add fixed point iteration method

---
+++ SquareRootMethod/Sources/IterationMethods/FixedPointIteration.swift
@@ -0,0 +1,44 @@
+//
+//  IterationMethods.swift
+//  
+//
+//  Created by Gleb Shevchenko on 5/31/20.
+//
+
+import Foundation
+
+/// - Reference: https://en.wikipedia.org/wiki/Fixed-point_iteration
+func fixedPointIteration(
+    x: Double, y: Double,
+    precision: Double
+) {
+    fixedPointIterationHelper(
+        x: x, y: y,
+        precision: precision,
+        iterationCount: 1
+    )
+}
+
+/// Helper for recursion.
+fileprivate func fixedPointIterationHelper(
+    x: Double, y: Double,
+    precision: Double, iterationCount: Int
+) {
+    let nextX = x
+        + C11 * (sin(y + 2) - y * x - 0.5)
+        + C12 * (x * x + y * y - 1)
+    let nextY = y
+        + C21 * (sin(y + 2) - y * x - 0.5)
+        + C22 * (x * x + y * y - 1)
+    
+    printIterationData(x: x, y: y, iterationCount: iterationCount)
+    if abs(y - nextY) < precision && abs(x - nextX) < precision {
+        return
+    } else {
+        fixedPointIterationHelper(
+            x: nextX, y: nextY,
+            precision: precision,
+            iterationCount: iterationCount + 1
+        )
+    }
+}
