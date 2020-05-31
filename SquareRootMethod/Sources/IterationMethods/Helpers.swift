e9c5e7e30c56f2c1f6f2fff2f398bb22469c1873
shgew <thegamedar@gmail.com>
2020-05-31 18:09:33 +0300

    Add Helpers

---
+++ SquareRootMethod/Sources/IterationMethods/Helpers.swift
@@ -0,0 +1,18 @@
+//
+//  Helpers.swift
+//  
+//
+//  Created by Gleb Shevchenko on 5/31/20.
+//
+
+let C11 = -0.184
+let C12 = -0.546
+let C21 = 0.654
+let C22 = 0.092
+
+func printIterationData(
+    x: Double, y: Double,
+    iterationCount: Int
+) {
+    print("x – \(x), y – \(y): \(iterationCount)")
+}
